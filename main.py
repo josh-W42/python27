ae67e8e018621d098647f78b81e154c106fa419e
Josh W <wilson.jd022@gmail.com>
2023-11-11 07:29:24 -0800

    Added additional ways to place objects in GUI and lightly touched padding

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ window.minsize(width=500, height=300)
 # Labels
 
 my_label = Label(text="This is a cool label", font=("Arial", 24, "bold"))
-my_label.pack()
+# my_label.pack()
 
 # How to update a tkinter label (Reminder, a label could just be text.)
 
@@ -26,11 +26,11 @@ def button_event():
 
 
 button = Button(text="Click Me", command=button_event)
-button.pack()
+# button.pack()
 
 text_entry = Entry(width=10)
 text_entry.insert(END, string="Default text")
-text_entry.pack()
+# text_entry.pack()
 
 # Text areas
 text_area = Text(height=5, width=30)
@@ -39,7 +39,7 @@ text_area.focus()
 text_area.insert(END, "Multi-line text entry")
 # Retrieves current value in textbox
 print(text_area.get("1.0", END))
-text_area.pack()
+# text_area.pack()
 
 
 # Spinbox (or a number incrementer or decrement(or)
@@ -48,7 +48,7 @@ def spinbox_used():
 
 
 spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
-spinbox.pack()
+# spinbox.pack()
 
 
 # Scales
@@ -57,7 +57,7 @@ def scale_used():
 
 
 scale = Scale(from_=0, to=100, command=scale_used)
-scale.pack()
+# scale.pack()
 
 
 # Checkboxes
@@ -67,7 +67,7 @@ def checkbox_used():
 
 checked_state = IntVar()
 check_button = Checkbutton(text="Is on?", variable=checked_state, command=checkbox_used)
-check_button.pack()
+# check_button.pack()
 
 
 # Radio buttons. to hold on to which radio button value is checked
@@ -79,8 +79,8 @@ radio_state = IntVar()
 radio_1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
 radio_2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
 
-radio_1.pack()
-radio_2.pack()
+# radio_1.pack()
+# radio_2.pack()
 
 
 # Listboxes
@@ -93,7 +93,39 @@ fruits = ["Apple", "Orange"]
 for i in range(len(fruits)):
     listbox.insert(i, fruits[i])
 listbox.bind("<<ListboxSelect>>", listbox_used)
-listbox.pack()
+# listbox.pack()
+
+# Pack, Place, and Grid
+"""
+    In tkinter there's three methods of
+    placing one of these complex objects in the GUI
+    
+    There's pack: which we've been using, which will just place the items
+    in the GUI one after another with not a lot of direction or control
+    
+    There's Place: Which is like the opposite, you have control over the
+    exact placement of the item.
+    
+    Then there's Grid: Which is like a flexible inbetween. Note that you cannot
+    utilize grid and pack in the same program.
+
+"""
+
+# my_label.pack()
+# my_label.place(x=10, y=100)
+# my_label.grid(row=0, column=0)
+
+#  Challenge: new orientation
+
+new_button = Button(text="New button")
+new_button.grid(row=0, column=3)
+
+my_label.grid(row=0, column=0)
+button.grid(row=1, column=1)
+text_entry.grid(row=2, column=4)
+
+# Padding - You can add padding like you would in HTML and CSS
+my_label.config(padx=100, pady=100)
 
 window.mainloop()
 
